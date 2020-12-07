03d69386d82711effbb6dd3d09f5fbd94159f4e4
Red_StonixX <72993807+RedSotnixX@users.noreply.github.com>
2020-12-07 21:47:52 +0100

    Update Scripttestv2.cs

--- Assets/Scripttestv2.cs
+++ Assets/Scripttestv2.cs
@@ -7,12 +7,12 @@ public class Scripttestv2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Salut ça va");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       print("Bienvenue");
     }
 }
