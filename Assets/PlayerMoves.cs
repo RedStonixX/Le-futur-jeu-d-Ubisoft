9f4cc0419923ee2e9c1a7dfd69aaa2b97eafce3a
Nicotsouga <75634814+Nicotsouga@users.noreply.github.com>
2020-12-10 21:40:36 +0100

    movesPlayer
    ajout de saut, déplacement, collider 2D et tileSheet

---
+++ Assets/PlayerMoves.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+public class PlayerMoves : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float jumpForce;
+
+    public bool isJumping;
+
+    public Rigidbody2D rb;
+    public SpriteRenderer spriteRenderer;
+
+    public float horizontalMovement;
+    private Vector3 velocity = Vector3.zero;
+
+    void FixedUpdate()
+    {;
+        PlayerDeplacement(horizontalMovement);
+    }
+
+
+    void Update()
+    {
+
+        horizontalMovement = Input.GetAxis("Horizontal") * MoveSpeed * Time.fixedDeltaTime;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+
+
+        Flip(rb.velocity.x);
+
+        float characterVelocity = Mathf.Abs(rb.velocity.x);
+    }
+
+    void PlayerDeplacement(float _horizontalMovement)
+    {
+        Vector3 targetVelocity = new Vector2(_horizontalMovement, rb.velocity.y);
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
+
+        if (isJumping == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            isJumping = false;
+        }
+    }
+    void Flip(float _velocity)
+    {
+        if (_velocity > 0.1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if (_velocity < -0.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
